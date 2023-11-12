716e1beef231f288720c350652de9fd63f59c8ea
limbluck <ffoxycide@gmail.com>
2023-11-12 20:07:57 +0300

    header tmplt upd WiP (dropdowns with close logic)

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,73 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 
 import { AuthorizationService } from '../../services/authorization.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  host: {
+    '(document:click)': 'onClick($event)'
+  }
 })
 export class HeaderComponent {
+  showSearchDropdown: boolean = false;
+  showNotificationsDropdown: boolean = false;
+
+  // Language dropdown control
+
+  showLanguageDropdown: boolean = false;
+
+  headerLanguage?: ElementRef;
+    @ViewChild('header__language') set headerLanguageSet(content: ElementRef) {
+      if (content) {
+        this.headerLanguage = content;
+      }
+    } 
+
+    headerLanguageDropdown?: ElementRef;
+    @ViewChild('header__languageDropdown') set headerLanguageDropdownSet(content: ElementRef) {
+      if (content) {
+        this.headerLanguageDropdown = content;
+      }
+    } 
+
+    toggleLanguageDropdown() {
+      this.showLanguageDropdown = !this.showLanguageDropdown;
+    }
+  
+  // Profile dropdown control
+
+    showProfileDropdown: boolean = false;
+
+    headerProfile?: ElementRef;
+    @ViewChild('header__profile') set headerProfileSet(content: ElementRef) {
+      if (content) {
+        this.headerProfile = content;
+      }
+    } 
+
+    headerProfileDropdown?: ElementRef;
+    @ViewChild('header__profileDropdown') set headerProfileDropdownSet(content: ElementRef) {
+      if (content) {
+        this.headerProfileDropdown = content;
+      }
+    } 
+
+    toggleProfileDropdown() {
+      this.showProfileDropdown = !this.showProfileDropdown;
+    }
+
+  // Onclick for the dropdown controls
+
+    onClick(event: MouseEvent) {
+      if (!this.headerProfile?.nativeElement.contains(event.target) && !this.headerProfileDropdown?.nativeElement.contains(event.target)) {
+        this.showProfileDropdown = false;
+      };
+      if (!this.headerLanguage?.nativeElement.contains(event.target) && !this.headerLanguageDropdown?.nativeElement.contains(event.target)) {
+        this.showLanguageDropdown = false;
+      };
+    }
+    
 
   // Authorization
     AuthorizationService: AuthorizationService = inject(AuthorizationService);
