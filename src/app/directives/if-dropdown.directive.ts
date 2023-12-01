0f2a58aef0e1c2ecdddfa43b367740e91ae53444
limbluck <ffoxycide@gmail.com>
2023-12-01 12:41:33 +0300

    upd IfDropdown (improve description)

--- src/app/directives/if-dropdown.directive.ts
+++ src/app/directives/if-dropdown.directive.ts
@@ -5,13 +5,29 @@ import { Directive, TemplateRef, ViewContainerRef, Input, Output, Renderer2, inj
 })
 export class IfDropdownDirective {
 /**
- * Structural directive for creating a dropdown element controlled by a parent element (button)
+ * Description:
+ * This structural directive is designed for creating a dropdown element controlled by a parent element, typically a button.
+ * It manages the visibility of the dropdown based on a boolean value and a toggle function provided by the parent component.
  *
  * @usageNotes
- * To use the directive, apply it to the ng-template, containing the dropdown element.
- * 
- * @param appIfDropdown - A boolean value from the parent component determining the visibility of the dropdown element.
- * @param appIfDropdownToggle - A function from the parent component that toggles the visibility of the dropdown element.
+ * To use this directive, apply it to an `ng-template` containing the dropdown element. The visibility of the dropdown is
+ * determinedo by the bolean value provided to the `appIfDropdown` input, and the dropdown is toggled using the function
+ * specified in the `appIfDropdownToggle` output.
+ *
+ * @example
+ * ```html
+ * <button (click)="toggleDropdown()">Toggle Dropdown</button>
+ * <ng-template [appIfDropdown]="isDropdownVisible" (appIfDropdownToggle)="toggleDropdown()">
+ *   <!-- Your dropdown content here -->
+ * </ng-template>
+ * ```
+ *
+ * @param {Input<boolean>} appIfDropdown - A boolean value from the parent component determining the visibility of the dropdown.
+ * @param {Output<void>} appIfDropdownToggle - An event emitted when the dropdown visibility should be toggled. Listen to this
+ * event in the parent component to perform the toggle action.
+ *
+ * @remarks
+ * In most this directive created to hold functionality for handling mouse events to close the dropdown when clicking outside of it.
  */
 
   private readonly renderer: Renderer2 = inject(Renderer2);
